9352ab0362f8e262466721b44707ed7b03b281da
mohitkmeena <mohitmeenaG2005@gmail.com>
2025-04-01 01:26:28 +0530

    investor side initialized

---
+++ src/main/java/com/mohit/Invoice_financing_investor/InvoiceFinancingInvestorApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.Invoice_financing_investor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFinancingInvestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceFinancingInvestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/Invoice_financing_investor/InvoiceFinancingInvestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.Invoice_financing_investor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceFinancingInvestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
